e6e00b6643d989a80f2522cf11b78ba526e73d9b
Himasankar <himasankar.t@gmail.com>
2017-02-11 17:04:40 +0530

    storetypes commit

---
+++ OnePos.API/Models/StoreTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace OnePos.API.Models
+{
+    public class StoreTypes
+    {
+        public int stroretypeid { get; set; }
+        public string storetypename { get; set; }
+        public string storetypedescription { get; set; }
+    }
+
+    public class StoreTypeResponse
+    {
+        public List<StoreTypes> storetypes { get; set; }
+        public string statusMessage { get; set; }
+        public HttpStatusCode statusCode { get; set; }
+    }
+}
